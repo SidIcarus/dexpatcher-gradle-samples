d0b10f9766c42361d88d7a51c186e74ba5e5936b
nachoapps <nachoappsinc@gmail.com>
2019-01-10 13:38:39 -0500

    Commit of initial b

---
+++ patched-app/patched/src/main/java/com/dnt7/padguide/c/b.java
@@ -0,0 +1,51 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.dnt7.padguide.c;
+
+public class b {
+    public static boolean a = false;
+    public static String b = "201302DNTMYQUEST";
+    public static final String c;
+    public static final String d;
+    public static final String e;
+    public static final String f;
+    public static final String g;
+    public static final String h;
+    public static final String i;
+    public static final String j;
+    public static final String k;
+    public static final String l;
+    public static final String m;
+    public static long n;
+    public static int o;
+    public static int p;
+    public static int q;
+
+    static {
+        String var0;
+        if (a) {
+            var0 = "http://paddev.dnt7.com";
+        } else {
+            var0 = "http://pad.dnt7.com";
+        }
+
+        c = var0;
+        d = c + "/api/";
+        e = c + "/images/";
+        f = c + "/upload/";
+        g = e + "icons/";
+        h = e + "icons/";
+        i = e + "wicons/";
+        j = e + "wicons/";
+        k = e + "youtube/";
+        l = e + "texture/";
+        m = f + "image_board/";
+        n = 1500L;
+        o = 3;
+        p = 3;
+        q = 3;
+    }
+}
