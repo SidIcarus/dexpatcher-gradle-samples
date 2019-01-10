4b494fbb4ab89f078bed3ac3755a6e4b8883a92b
nachoapps <nachoappsinc@gmail.com>
2019-01-10 13:37:46 -0500

    Commit of adjusted RegistrationIntentService

--- patched-app/patched/src/main/java/com/dnt7/padguide/service/RegistrationIntentService.java
+++ patched-app/patched/src/main/java/com/dnt7/padguide/service/RegistrationIntentService.java
@@ -1,72 +1,25 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package com.dnt7.padguide.service;
 
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import com.dnt7.padguide.c.e;
-import com.dnt7.padguide.c.g;
-import com.dnt7.padguide.db.a.bd;
-import com.dnt7.padguide.e.b;
-import com.dnt7.padguide.e.b.a;
-import com.dnt7.padguide.f.k;
-import java.io.IOException;
-import java.util.HashMap;
 
+import lanchon.dexpatcher.annotation.DexEdit;
+import lanchon.dexpatcher.annotation.DexIgnore;
+import lanchon.dexpatcher.annotation.DexReplace;
+
+@DexEdit
 public class RegistrationIntentService extends IntentService {
+    @DexIgnore
     public RegistrationIntentService() {
         super("PadGuide");
     }
 
+    @DexReplace
     public static void a(Context var0, String var1) {
-        if (k.d(var1)) {
-            bd.b(var0, "PUSH_TOKEN", var1);
-        }
-
-        String var2 = bd.a(var0, "PUSH_TOKEN", "");
-        if (k.d(var2)) {
-            HashMap var5 = new HashMap();
-            var5.put("GID", "3");
-            var5.put("DEVICE_ID", var2);
-            var5.put("USER_KEY", e.s(var0));
-            var5.put("OS", "A");
-            var5.put("SERVER", e.f(var0));
-
-            for(int var3 = 0; var3 < 6; ++var3) {
-                byte var4;
-                if (var3 == 5) {
-                    var4 = 0;
-                } else {
-                    var4 = 1;
-                }
-
-                int var6 = bd.b(var0, k.a("PUSH_SETTING_%02d", new Object[]{var3}), var4);
-                var5.put(k.a("PUSH_%02d", new Object[]{var3}), k.a("%d", new Object[]{var6}));
-            }
-
-            b.a(var0, "http://api.gamekiji.com/api/regPushSetting.jsp", var5, (a)null);
-        } else {
-            g.a("PadGuide", "Push Token Error");
-        }
-
     }
 
+    @DexIgnore
     protected void onHandleIntent(Intent var1) {
-        com.google.android.gms.iid.a var3 = com.google.android.gms.iid.a.c(this);
-
-        try {
-            String var4 = var3.a(this.getString(2131100366), "GCM", (Bundle)null);
-            if (!var4.equals(bd.a(this, "PUSH_TOKEN", ""))) {
-                a(this, var4);
-            }
-        } catch (IOException var2) {
-            g.b("PadGuide", var2);
-        }
-
     }
 }
