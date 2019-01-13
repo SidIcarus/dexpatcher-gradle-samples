36a4a7d62f2cfefc01f548e286ddd52fa5795678
nachoapps <nachoappsinc@gmail.com>
2019-01-12 19:52:56 -0500

    Disable the 'high quality' setting

--- patched-app/patched/src/main/java/com/dnt7/padguide/d/au.java
+++ patched-app/patched/src/main/java/com/dnt7/padguide/d/au.java
@@ -5,29 +5,67 @@
 
 package com.dnt7.padguide.d;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dnt7.padguide.b.b;
 
 import lanchon.dexpatcher.annotation.DexAction;
+import lanchon.dexpatcher.annotation.DexAdd;
 import lanchon.dexpatcher.annotation.DexEdit;
 import lanchon.dexpatcher.annotation.DexIgnore;
 import lanchon.dexpatcher.annotation.DexPrepend;
+import lanchon.dexpatcher.annotation.DexWrap;
 
 @DexEdit(defaultAction = DexAction.IGNORE)
 public class au extends b implements OnClickListener {
+    @DexIgnore
+    private Context g = null;
+    @DexIgnore
+    private TextView bE = null;
+
     @DexEdit
     boolean b = true;
 
-    // Set 'purchased' to true before the 'check purchased' function (easier to hook this way)
+
     @DexPrepend
     void V() {
+        // Set 'purchased' to true before the 'check purchased' function (easier to hook this way)
         b = true;
     }
 
     @DexIgnore
     public void onClick(View var1) {
         // per interface requirements
+    }
+
+    // The 'image quality' setting on dadguide is counterproductive. Setting it to 'low' results
+    // in a better user experience. Doing some magic to auto toggle it from high, and preventing
+    // users from changing it back.
+    @DexWrap()
+    public View a(LayoutInflater param1, ViewGroup param2, Bundle param3) {
+        // The original 'a' is the onCreate method.
+        // Hook it to toggle the image quality setting to low if necessary.
+        View v = a(param1, param2, param3);
+        String imageQuality = bE.getText().toString();
+        if (imageQuality.equals(this.g.getResources().getString(2131099753))) {
+            // The image quality was high, so call the original ad() to toggle it to low.
+            source_ad();
+        }
+        return v;
+    }
+
+    // Rename the method that toggles the image quality to low/high and adjusts the textview.
+    @DexEdit(target = "ad")
+    private void source_ad() { throw null; }
+
+    // Add a new stub method in place of ad() which does nothing on click.
+    @DexAdd
+    private void ad() {
     }
 }
